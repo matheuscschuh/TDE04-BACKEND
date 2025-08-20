3473be617a41dddc9b973fd00cf30032e7601196
matheuscschuh <matheus.cschuh@gmail.com>
2025-08-19 21:38:30 -0300

    ddd

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿double saldo = 1000.00;
+int opcao;
+do
+{
+    Console.WriteLine("1 - Ver Saldo");
+    Console.WriteLine("2 - Depositar");
+    Console.WriteLine("3 - Sacar");
+    Console.WriteLine("4 - Sair");
+
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine($"O seu saldo disponivel é: {saldo}");
+            break;
+        case 2:
+            Console.WriteLine("Digite o valor que queira depositar:");
+            double depositar = Convert.ToDouble(Console.ReadLine());
+            if (depositar < 0)
+            {
+                saldo = saldo + depositar;
+                Console.WriteLine("Deposito realizado com sucesso!");
+
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido!");
+            }
+            break;
+        case 3:
+            Console.WriteLine("Digite o valor que queira sacar:");
+            double sacar = Convert.ToDouble(Console.ReadLine());
+            if (sacar <= saldo)
+            {
+                saldo -= sacar;
+                Console.WriteLine("Saque realizado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+            break;
+        case 4:
+            Console.WriteLine("Obrigado por usar nosso sistema!");
+            break;
+        default:
+            Console.WriteLine("Valor invalido!");
+            break;
+    }
+} while (opcao != 4);
